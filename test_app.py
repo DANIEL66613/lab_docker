ad7be368a97e65e697a9439bf28cbdbc60dcd65d
DANIEL66613 <104590003+DANIEL66613@users.noreply.github.com>
2024-11-11 19:51:06 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,19 +2,18 @@ import unittest
 from app import app
 import werkzeug
 
-# Patch temporário para adicionar o atributo '__version__' em werkzeug
 if not hasattr(werkzeug, '__version__'):
     werkzeug.__version__ = "mock-version"
 
 class APITestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-      cls.client = app.test_client()
+        cls.client = app.test_client()
 
     def test_get_items(self):
         response = self.client.get('/items')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"items": ["item1", "item2", "item3"]})
+        self.assertEqual(response.get_json(), {"items": ["item1", "item2", "item3"]})
 
     def test_swagger_ui(self):
         response = self.client.get('/swagger')
@@ -25,7 +24,7 @@ class APITestCase(unittest.TestCase):
         invalid_token = "invalid_token_example"
         response = self.client.get('/protected', headers={'Authorization': f'Bearer {invalid_token}'})
         self.assertEqual(response.status_code, 401)
-        self.assertIn('The token is invalid', response.json['msg'])
+        self.assertIn('The token is invalid', response.get_json()['msg'])
 
 if __name__ == '__main__':
     unittest.main()
